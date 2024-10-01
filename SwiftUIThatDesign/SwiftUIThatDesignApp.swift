e596827bf1ec84f6b46de2c0eef79358947ff16b
Tom Phillips <tomphillipsmusic@gmail.com>
2024-10-01 08:33:07 -0400

    Initial Commit

---
+++ SwiftUIThatDesign/SwiftUIThatDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIThatDesignApp.swift
+//  SwiftUIThatDesign
+//
+//  Created by Tom Phillips on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIThatDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
